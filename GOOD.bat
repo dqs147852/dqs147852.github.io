6e6e15234a10224a63d91f70500d2b936d977f95
dqs147852 <145889246+dqs147852@users.noreply.github.com>
2024-01-20 23:24:30 +0800

    Update GOOD.bat

--- GOOD.bat
+++ GOOD.bat
@@ -1,28 +1,19 @@
 @echo off
 chcp 65001
-title 创建病毒
-echo hi，病毒创建中
-echo @echo off> sb.bat
-echo echo...> sb.bat
-echo timeout /t 0.2 >nul> sb.bat
-echo echo 你为什么要打开我啊...> sb.bat
-echo echo 我给你看个网页吧？> sb.bat
-echo echo start https://dqs147852.github.io/gyc> sb.bat
-echo  timeout /t 5 >nul> sb.bat
-echo echo 没错，是这个远古网页> sb.bat
-echo echo 唉，算了，没意思，5秒后我就把自己关了叭> sb.bat
-echo timeout /t 5 />nul> sb.bat
 
-timeout /t 5 >nul
-echo 病毒创建完毕！
+title 只有super boy 才会打开
+setlocal enabledelayedexpansion
 
-
-
-set /p choice=是否要打开该文件？(Y/N):
-
-IF "%choice%"=="Y" (
-    start "" "sb.bat"
-) ELSE (
-    echo 感谢使用！
+set idx=0
+for /L %%i in (1,1,10000) do (
+    set /a idx=!idx!+1
+    set color=!idx!
+    if !color! gtr 15 set idx=0
+    color !color!
+    echo 我是病毒!
+    timeout /t 0.3 /nobreak >nul
 )
-pause
+
+color 07
+echo Done!
+pause >nul
